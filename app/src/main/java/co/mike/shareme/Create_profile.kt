572e49f5fc188fbc0e6f4510bf57b47c72226bd0
miguel.alegria <mikel063093@gmail.com>
2017-06-14 16:19:09 -0500

    Fix

--- app/src/main/java/co/mike/shareme/Create_profile.kt
+++ app/src/main/java/co/mike/shareme/Create_profile.kt
@@ -5,6 +5,8 @@ import android.app.Activity
 import android.databinding.DataBindingUtil
 import android.net.Uri
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import co.mike.shareme.base.BaseActivity
 import co.mike.shareme.databinding.ActivityCreateProfileBinding
 import co.mike.shareme.util.Logger
@@ -18,12 +20,31 @@ import io.reactivex.schedulers.Schedulers
 class Create_profile : BaseActivity() {
 
   lateinit var binding: ActivityCreateProfileBinding
+  var localImage: FileData? = null
+
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     Logger.log("CreateProfile", "onCrate")
     binding = DataBindingUtil.setContentView(this, R.layout.activity_create_profile)
-    //binding.btnNext.setOnClickListener({ loadPhoto() })
+    binding.btnNext.setOnClickListener(
+        {
+          Logger.log("click Next with image ${localImage?.filename}, name ${binding.edtName.text}")
+        })
     binding.imgPicture.setOnClickListener({ loadPhoto() })
+    binding.edtName.addTextChangedListener(object : TextWatcher {
+      override fun afterTextChanged(txt: Editable?) {
+      }
+
+      override fun beforeTextChanged(txt: CharSequence?, p1: Int, p2: Int, p3: Int) {
+      }
+
+      override fun onTextChanged(txt: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        binding.isLoading = txt?.length!! >= 3
+        Logger.log("onTextChanged $txt")
+      }
+    })
+
   }
 
   private fun loadPhoto() {
@@ -34,7 +55,6 @@ class Create_profile : BaseActivity() {
         .observeOn(AndroidSchedulers.mainThread())
         .subscribe({ response ->
           if (Activity.RESULT_OK != response.resultCode()) {
-
             Logger.log("load image cancel")
             return@subscribe
           }
@@ -44,6 +64,7 @@ class Create_profile : BaseActivity() {
   }
 
   private fun loadImage(file: FileData) {
+    localImage = file
     val uri = Uri.fromFile(file.file)
     binding.imgPicture.setImageURI(uri)
   }
